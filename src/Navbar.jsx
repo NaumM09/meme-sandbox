c2caf5311b759ce1f6f8c53573a7bd63d11f44b4
Naum <97293336+NaumM09@users.noreply.github.com>
2024-11-22 10:53:43 +0200

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { FaTelegramPlane, FaTwitter } from 'react-icons';
-import './App.css';
+//import React from 'react';
+//import { FaTelegramPlane, FaTwitter } from 'react-icons';
+//import './App.css';
 
-const Navbar = () => {
-  return (
-    <nav className="centered-navbar">
-      <a
-        href="https://t.me/memecoinbeginner"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="nav-icon"
-        aria-label="Telegram"
-      >
-        <FaTelegramPlane />
-      </a>
-      <a
-        href="https://x.com/startermemez"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="nav-icon"
-        aria-label="X (Twitter)"
-      >
-        <FaXTwitter />
-      </a>
-    </nav>
-  );
-};
+//const Navbar = () => {
+  //return (
+   // <nav className="centered-navbar">
+      //<a
+       // href="https://t.me/memecoinbeginner"
+       // target="_blank"
+       // rel="noopener noreferrer"
+        //className="nav-icon"
+     //   aria-label="Telegram"
+  //    >
+      //  <FaTelegramPlane />
+    //  </a>
+   //   <a
+   //     href="https://x.com/startermemez"
+   //     target="_blank"
+   //     rel="noopener noreferrer"
+  //      className="nav-icon"
+  //      aria-label="X (Twitter)"
+   //   >
+//        <FaXTwitter />
+//      </a>
+//    </nav>
+//  );
+//};
 
-export default Navbar;
+//export default Navbar; 
+
 
