054fd783c2d3256aa6798b0747ea20700c4c50f6
Naum <97293336+NaumM09@users.noreply.github.com>
2024-11-22 11:01:34 +0200

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,32 +1,3 @@
-//import React from 'react';
-//import { FaTelegramPlane, FaTwitter } from 'react-icons';
-//import './App.css';
 
-//const Navbar = () => {
-  //return (
-   // <nav className="centered-navbar">
-      //<a
-       // href="https://t.me/memecoinbeginner"
-       // target="_blank"
-       // rel="noopener noreferrer"
-        //className="nav-icon"
-     //   aria-label="Telegram"
-  //    >
-      //  <FaTelegramPlane />
-    //  </a>
-   //   <a
-   //     href="https://x.com/startermemez"
-   //     target="_blank"
-   //     rel="noopener noreferrer"
-  //      className="nav-icon"
-  //      aria-label="X (Twitter)"
-   //   >
-//        <FaXTwitter />
-//      </a>
-//    </nav>
-//  );
-//};
-
-//export default Navbar; 
 
 
