4dc547c2756f317c518ef1e7c54c3e32ee3bad24
Naum <97293336+NaumM09@users.noreply.github.com>
2024-11-22 03:32:47 +0200

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="centered-navbar">
       <a
-        href="https://telegram.org"
+        href="https://t.me/memecoinbeginner"
         target="_blank"
         rel="noopener noreferrer"
         className="nav-icon"
@@ -14,7 +14,7 @@ const Navbar = () => {
         <i className="fab fa-telegram-plane"></i>
       </a>
       <a
-        href="https://x.com"
+        href="https://x.com/startermemez"
         target="_blank"
         rel="noopener noreferrer"
         className="nav-icon"
