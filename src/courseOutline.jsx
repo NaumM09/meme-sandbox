f68a2670289957d59a943a1dd321654c61d036ba
Naum <97293336+NaumM09@users.noreply.github.com>
2024-11-24 21:31:51 +0200

    Update courseOutline.jsx

--- src/courseOutline.jsx
+++ src/courseOutline.jsx
@@ -5,32 +5,32 @@ const courseData = [
   {
     section: "Introduction",
     lessons: [
-      { title: "Introduction To Meme Coin"},
+      { title: "Introduction To Meme Coin" },
       { title: "Memecoin Terminology" },
-      { title: "Setting up Wallet"},
+      { title: "Setting up Wallet" },
     ],
   },
   {
     section: "Understanding Meme Coins & Technical Analysis",
     lessons: [
-    { title: "Types of Trading Platforms & Bots"},
+      { title: "Types of Trading Platforms & Bots" },
       { title: "Identifying Trading Opportunities" },
-      { title: "Understanding Slippage & Trade Execution"},
-      { title: "Rugs, Pulls and Jeets"},
-      { title: "How To Analyse Meme Coins"},
-      { title: "Understanding Long Term Meme Coin Pumps & Narratives"},
-      { title: "Liquidity, Market Cap & Price"},
-      { title: "Technical Analysis (TA)"},
-      { title: "New Meme Coin Launches "},
+      { title: "Understanding Slippage & Trade Execution" },
+      { title: "Rugs, Pulls and Jeets" },
+      { title: "How To Analyse Meme Coins" },
+      { title: "Understanding Long Term Meme Coin Pumps & Narratives" },
+      { title: "Liquidity, Market Cap & Price" },
+      { title: "Technical Analysis (TA)" },
+      { title: "New Meme Coin Launches" },
       { title: "How to Avoid Shitcoins" },
     ],
   },
   {
     section: "Bonus Content",
     lessons: [
-    { title: "The Power of Community"},
-      { title: "Finding Whale Accounts"},
-      { title: "Influencer Marketing & Meme Coin Scams"},
+      { title: "The Power of Community" },
+      { title: "Finding Whale Accounts" },
+      { title: "Influencer Marketing & Meme Coin Scams" },
     ],
   },
 ];
@@ -46,7 +46,6 @@ const CourseOutline = () => {
               <li key={idx} className="lesson-item">
                 <div className="lesson-details">
                   <span className="lesson-title">{lesson.title}</span>
-                  <span className="lesson-duration">({lesson})</span>
                 </div>
                 <button className="start-btn">Purchase</button>
               </li>
