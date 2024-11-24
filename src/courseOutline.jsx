534a23240b0399ae1f7d80dd21c223283bbce640
Naum <97293336+NaumM09@users.noreply.github.com>
2024-11-24 21:21:01 +0200

    Add files via upload

---
+++ src/courseOutline.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './App.css';
+
+const courseData = [
+  {
+    section: "Introduction",
+    lessons: [
+      { title: "Introduction To Meme Coin"},
+      { title: "Memecoin Terminology" },
+      { title: "Setting up Wallet"},
+    ],
+  },
+  {
+    section: "Understanding Meme Coins & Technical Analysis",
+    lessons: [
+    { title: "Types of Trading Platforms & Bots"},
+      { title: "Identifying Trading Opportunities" },
+      { title: "Understanding Slippage & Trade Execution"},
+      { title: "Rugs, Pulls and Jeets"},
+      { title: "How To Analyse Meme Coins"},
+      { title: "Understanding Long Term Meme Coin Pumps & Narratives"},
+      { title: "Liquidity, Market Cap & Price"},
+      { title: "Technical Analysis (TA)"},
+      { title: "New Meme Coin Launches "},
+      { title: "How to Avoid Shitcoins" },
+    ],
+  },
+  {
+    section: "Bonus Content",
+    lessons: [
+    { title: "The Power of Community"},
+      { title: "Finding Whale Accounts"},
+      { title: "Influencer Marketing & Meme Coin Scams"},
+    ],
+  },
+];
+
+const CourseOutline = () => {
+  return (
+    <section className="course-outline">
+      {courseData.map((section, index) => (
+        <div key={index} className="course-section">
+          <h3 className="section-title">{section.section}</h3>
+          <ul className="lesson-list">
+            {section.lessons.map((lesson, idx) => (
+              <li key={idx} className="lesson-item">
+                <div className="lesson-details">
+                  <span className="lesson-title">{lesson.title}</span>
+                  <span className="lesson-duration">({lesson})</span>
+                </div>
+                <button className="start-btn">START</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default CourseOutline;
